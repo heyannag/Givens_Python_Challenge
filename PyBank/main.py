a7bc0ba6aaa6417147ce9630848e88f0051a565a
heyannag <61209602+heyannag@users.noreply.github.com>
2020-03-28 23:55:30 -0500

    Update file name

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,72 +1,80 @@
-# Import modules
+# Objective, create a Python script to run the following:
+# The total number of months included in the dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "Profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over the entire period
+
+
+#Start by importing the dependencies
 import os
 import csv
 
-def getthestuff(csv):
+# Defining initial values of variables
+months = 0
+net_total = 0
+
+# Start by creating a csv variable to easily reference the data
+def PyBank(data):
+    
+    # Collect the total number of months
     months = 0
-    total = 0
-    maxrev = 0
-    minrev = 0
-    avgchange = 0
-    maxmonth = ""
-    minmonth = ""
-    lastmonth = None
-    changes = []
-    for row in csv:
-        current_month = row[0]
-        current_pnl = int(row[1])
-        total += current_pnl
+    # Record the "Profit/Losses" under net total
+    net_total = 0
+    # Make a list of the weekly profit/losses
+    weekly_amount = []
+    # Make a list of each week
+    weeks = []
+
+    # Begin For loop, for every row in csv:
+    for row in data:
         months += 1
-        if lastmonth is not None:
-            current_change = current_pnl-lastmonth
-            changes.append(current_change)
-            if current_change > maxrev:
-                maxrev = current_change
-                maxmonth = current_month
-            if current_change < minrev:
-                minrev = current_change
-                minmonth = current_month
+        net_total = net_total + int(row[1])
+        weekly_amount.append(row[1])
+        weeks.append(row[0])
 
-        lastmonth = current_pnl
-    avgchange = sum(changes)/len(changes)
-    return [months, total, maxrev, minrev, avgchange, maxmonth, minmonth]
+    # Configure a list to show the changes week over week week. This will be used to caluclate the average, min, and max.
+    weekly_change = []
+    # Begin 'For loop' within the range of weekly_amount
+    for i in range(len(weekly_amount)-1):
+        weekly_change.append(int(weekly_amount[i+1])-int(weekly_amount[i]))
+    
+    # Begin 'For loop' to record a list of the weekly_change that will be used to determine min/max
+    for j in range(len(weekly_change)-1): 
+        if j == 0:
+            # State the max and min values
+            max_change = weekly_change[0]
+            min_change = weekly_change[0]
+        else: 
+            if weekly_change[j] > max_change:
+                max_change = weekly_change[j]
+                max_week = weeks[j+1]
+            elif weekly_change[j] < min_change:
+                min_change = weekly_change[j]
+                min_week = weeks[j+1]
+            else: #last else statement 
+                max_change = max_change
+                min_change = min_change
 
-with open("budget_data.csv") as file:
-    csvreader = csv.reader(file, delimiter = ',')
+    avg_change  = round(sum(weekly_change)/len(weekly_change),2)
+
+    return [months,net_total,avg_change,max_change,min_change,max_week,min_week]
+
+
+#Set the path for my csv file
+csvpath = os.path.join('Resources','budget_data.csv')
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    #Skip the header line
     header = next(csvreader)
-    analysis = getthestuff(csvreader)
-print(f"""
-Financial Analysis
------------------
-Total Months: {analysis[0]}
-Total: ${round(analysis[1], 2)}
-Average Change: ${round(analysis[4], 2)}
-Greatest Increase: {analysis[5]} (${analysis[2]})
-Greatest Decrease: {analysis[6]} (${analysis[3]})
-""")
+    analysis = PyBank(csvreader)
 
-arr = [2, 4, 34, 6, 21, 18, 24, 29, 31, 18, 48]
+print(f"Total months: {analysis[0]}\nNet Total Profit/Loss: ${analysis[1]}\nAverage Weekly Change: ${analysis[2]}\nMax Weekly Change: {analysis[5]} (${analysis[3]})\nMin Weekly Change: {analysis[6]} (${analysis[4]})")
 
-arr
-
-def sort_array(arr):
-  sorted = []
-  for value in arr:
-    if len(sorted) == 0:
-      sorted.append(value)
-    else:
-      i = 0
-      for ind in sorted:
-        if value > ind:
-          i+=1
-        else:
-          sorted.insert(i-1, value)
-          break
-        if i == len(sorted):
-          sorted.append(value)
-          break
-  return sorted
-
-sort_array(arr)
-# Set output file path
-# Save the stuff to a text file
+with open("output_file.txt","w") as text_file:
+    text_file.write(f"Total months: {analysis[0]}\nNet Total Profit/Loss: ${analysis[1]}\nAverage Weekly Change: ${analysis[2]}\nMax Weekly Change: {analysis[5]} (${analysis[3]})\nMin Weekly Change: {analysis[6]} (${analysis[4]})")
+             
